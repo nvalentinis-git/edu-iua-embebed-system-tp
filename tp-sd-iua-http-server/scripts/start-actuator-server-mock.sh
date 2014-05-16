842b76ef35c79c3590d3941dca9d6bd6249ce5bf
Nestor Valentinis <Nestor.Valentinis@wnco.com>
2014-05-16 15:44:36 -0500

    change the actuator scrip

--- tp-sd-iua-http-server/scripts/start-actuator-server-mock.sh
+++ tp-sd-iua-http-server/scripts/start-actuator-server-mock.sh
@@ -10,10 +10,28 @@ if [[ -n $1 ]]; then
 	ACTUATOR_PORT_NUMBER=$1    
 fi
 
+NETWORK_CONFIG=" -DactuatorClientPortNumber=$ACTUATOR_PORT_NUMBER "
+
+# Configuration Actuator Protocol
+actuatorClientLogicalJson=getLogicValueJSON
+actuatorClientLogicalXml=getLogicValueXML
+actuatorClientAnalogicalJson=getAnalogicValueJSON
+actuatorClientAnalogicalXml=getAnalogicValueXML
+
+PROTOCOL_CONFIG=" -DactuatorClientLogicalJson=$actuatorClientLogicalJson -DactuatorClientLogicalXml=$actuatorClientLogicalXml -DactuatorClientAnalogicalJson=$actuatorClientAnalogicalJson -DactuatorClientAnalogicalXml=$actuatorClientAnalogicalXml "
+
 echo Actuator Port Number used: $ACTUATOR_PORT_NUMBER
+echo --------------------------------------------------------------
+
+echo Actuator Protocol Configuration: 
+echo 	$actuatorClientLogicalJson
+echo 	$actuatorClientLogicalXml
+echo 	$actuatorClientAnalogicalJson
+echo 	$actuatorClientAnalogicalXml
+echo --------------------------------------------------------------
 
 echo Starting Java App...
 echo .................
 
 #chmod a+x *.sh
-java -cp ../target/tp-sd-iua-http-server-1.0-SNAPSHOT-jar-with-dependencies.jar -DactuatorClientPortNumber=$ACTUATOR_PORT_NUMBER ar.edu.iua.practicoSD.actuatorServer.ActuatorServerMock
+java -cp ../target/tp-sd-iua-http-server-1.0-SNAPSHOT-jar-with-dependencies.jar $NETWORK_CONFIG $PROTOCOL_CONFIG ar.edu.iua.practicoSD.actuatorServer.ActuatorServerMock
